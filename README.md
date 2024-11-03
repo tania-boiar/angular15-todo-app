d8894ac272f521f96a04ae39951de154810102e5
Tetiana Boiar <taniavasylchyk@gmail.com>
2024-11-03 13:53:46 +0200

    gh-deployment fix

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# MyAngular15App
+# Angular15TodoApp
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 15.2.11.
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-angular15-app';
+  title = 'angular-todo-app';
 }
