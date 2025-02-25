b441676534b484886989a2b59e6a1973724f2cdc
Tetiana Boiar <taniavasylchyk@gmail.com>
2025-02-25 02:46:48 +0200

    edit & save

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, OnChanges, Output, SimpleChanges, ViewChild } from '@angular/core';
 import { Todo } from 'src/app/types/todo';
 
 @Component({
@@ -7,7 +7,30 @@ import { Todo } from 'src/app/types/todo';
   styleUrls: ['./todo.component.scss']
 })
 export class TodoComponent {
+  @Output() delete = new EventEmitter;
   @Input() todo!: Todo;
 
+  @ViewChild('titleField')
+  set titleField(field: ElementRef) {
+    if(field) {
+      field.nativeElement.focus();
+    }
+  };
+
   editing = false;
+  title = '';
+
+  edit() {
+    this.editing = true;
+    this.title = this.todo.title;
+  }
+
+  save() {
+    if (!this.editing) {
+      return;
+    }
+
+    this.editing = false;
+    this.todo.title = this.title;
+  }
 }
